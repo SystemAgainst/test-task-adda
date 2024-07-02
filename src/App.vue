4703b7d62488a76fc2f1f51b8bf7f8f9b4c1bfd9
Tigran Dzhamalyan <tigr.SA2021@gmail.com>
2024-07-02 12:01:07 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="left-panel">
+    <div class="list">
+      <div class="list-header">
+        <h1>List 1</h1>
+        <font-awesome-icon icon="chevron-down"/>
+      </div>
+
+      <div class="list-item">
+        <input type="checkbox" />
+        <span>sadf</span>
+        <input class="list-item__input-number" type="number"/>
+        <input class="list-item__input-color" type="color" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.left-panel {
+  width: 350px;
+  padding: 20px;
+  background-color: #fff;
+  box-shadow: 0 2px 4px rgba(0,0,0,0.1);
+  border-radius: 8px;
+}
+
+.list-header {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.list-item {
+  display: flex;
+  align-items: center;
+  gap: .5rem;
+}
+
+.list-item__input-color {
+  width: 40px;
+  height: 40px;
+  border: none;
+}
+
+.list-item__input-color,
+.list-item__input-number {
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  padding: 5px;
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,25 @@
+import './assets/style.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import App from './App.vue'
+
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faChevronDown)
+
+const pinia = createPinia()
+const app = createApp(App)
+
+app.use(pinia)
+app.component('font-awesome-icon', FontAwesomeIcon)
+app.mount('#app')
